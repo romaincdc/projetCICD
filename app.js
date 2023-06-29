280e4e281d1ab0b6ee7f7965045f7e306e08482c
romaincdc <romaincdc@gmail.com>
2023-06-29 17:37:36 +0200

    update app.js error azure

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const http = require('http');
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hello world');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
